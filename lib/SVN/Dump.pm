9c1d87bf8898c55b11c6e0cf9f84fb6a24d48936
Philippe Bruhat (BooK) <book@cpan.org>
2006-10-18 08:33:22 -0800

    initial import of SVN-Dump 0.01 from CPAN
    git-cpan-module:   SVN-Dump
    git-cpan-version:  0.01
    git-cpan-authorid: BOOK
    git-cpan-file:     authors/id/B/BO/BOOK/SVN-Dump-0.01.tar.gz

---
+++ lib/SVN/Dump.pm
@@ -0,0 +1,151 @@
+package SVN::Dump;
+
+use strict;
+use warnings;
+use Carp;
+
+use SVN::Dump::Reader;
+
+our $VERSION = '0.01';
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    # create the Reader object now
+    my $self = bless {}, $class;
+    open my $fh, $args->{file} or croak "Can't open $args->{file}: $!";
+    $self->{reader} = SVN::Dump::Reader->new( $fh );
+
+    return $self;
+}
+
+sub next_record {
+    my ($self) = @_;
+    my $record;
+
+RECORD: {
+        $record = $self->{reader}->read_record();
+        return unless $record;
+
+        # keep the first records in the dump itself
+        my $type = $record->type();
+        if ( $type =~ /\A(?:format|uuid)\z/ ) {
+            $self->{$type} = $record;
+        }
+    }
+
+    return $record;
+}
+
+sub version {
+    my ($self) = @_;
+    return $self->{format}
+        ? $self->{format}->get_headers()->{'SVN-fs-dump-format-version'}
+        : '';
+}
+
+sub uuid {
+    my ($self) = @_;
+    return $self->{uuid} ? $self->{uuid}->get_headers()->{UUID} : '';
+}
+
+sub as_string {
+    return join '', $_[0]->{$_}->as_string() for (qw( format uuid ));
+}
+
+__END__
+
+=head1 NAME
+
+SVN::Dump - A Perl interface to Subversion dumps
+
+=head1 SYNOPSIS
+
+    use SVN::Dump;
+    
+    my $file = shift;
+    my $dump = SVN::Dump->new( { file => $file } );
+    
+    # compute some stats
+    my %type;
+    while ( my $record = $dump->next_record() ) {
+        $type{ $record->type() }++;
+    }
+    
+    # print the results
+    print "Dump $file statistics:\n",
+          "  version:   ", $dump->version(), "\n",
+          "  uuid:      ", $dump->uuid(), "\n",
+          "  revisions: ", $type{revision}, "\n",
+          "  nodes:     ", $type{node}, "\n";
+
+=head1 DESCRIPTION
+
+B<This module is an alpha release. The interfaces will probably change
+in the future, as I slowly learn my way inside the SVN dump format.>
+
+An C<SVN::Dump> object represents a Subversion dump.
+
+This module follow the semantics used in the reference document
+(the file F<notes/fs_dumprestore.txt> in the Subversion source tree).
+
+Each class has a C<as_string()> method that prints its content
+in the dump format.
+
+The most basic thing you can do with C<SVN::Dump> is simply copy
+a dump:
+
+    use SVN::Dump;
+
+    my $dump = SVN::Dump->new( 'mydump.svn' );
+    print $dump->as_string(); # only print the dump header
+
+    while( $rev = $dump->next_record() ) {
+        print $rev->as_string();
+    }
+
+After the operation, the resulting dump should be identical to the
+original dump.
+
+=head1 METHODS
+
+C<SVN::Dump> provides the following methods:
+
+=over 4
+
+=item new()
+
+Return a new C<SVN::Dump> object.
+
+=item next_record()
+
+Return the next record read from the dump.
+
+=item version()
+
+Return the dump format version, if the version record has already been read.
+
+=item uuid()
+
+Return the dump UUID, if there is an UUID record and it has been read.
+
+=item as_string()
+
+Return a string representation of the dump specific blocks
+(the C<format> and C<uuid> blocks only).
+
+=back
+
+=head1 SEE ALSO
+
+C<SVN::Dump::Reader>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 Philippe 'BooK' Bruhat, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
---
+++ lib/SVN/Dump/Record.pm
@@ -0,0 +1,186 @@
+package SVN::Dump::Record;
+
+use strict;
+use warnings;
+
+my $NL = "\012";
+
+sub new {
+    my ($class, @args) = @_;
+    return bless {}, $class
+}
+
+for my $attr (qw( headers property text included )) {
+    no strict 'refs';
+    *{"set_$attr"} = sub { $_[0]->{$attr} = $_[1]; };
+    *{"get_$attr"} = sub { $_[0]->{$attr} };
+}
+
+sub type {
+    my ($self) = @_;
+    return $self->{headers} ? $self->{headers}->type() : '';
+}
+
+sub has_text { return exists $_[0]->{headers}{'Text-content-length'} }
+sub has_prop { return exists $_[0]->{headers}{'Prop-content-length'} }
+sub has_prop_only {
+    return exists $_[0]->{headers}{'Prop-content-length'}
+        && !exists $_[0]->{headers}{'Text-content-length'};
+}
+sub has_prop_or_text {
+    return exists $_[0]->{headers}{'Prop-content-length'}
+        || exists $_[0]->{headers}{'Text-content-length'};
+}
+
+sub as_string {
+    my ($self) = @_;
+    my $headers = $self->get_headers();
+
+    # the headers
+    my $string = $headers->as_string();
+
+    # the properties
+    $string .= $self->get_property()->as_string()
+        if exists $headers->{'Prop-content-length'};
+
+    # the text
+    $string .= $self->get_text()->as_string()
+        if exists $headers->{'Text-content-length'};
+
+    # is there an included record?
+    if( my $included = $self->get_included() ) {
+        $string .= $included->as_string() . $NL;
+    }
+
+    # add a record separator if needed
+    my $type = $self->type();
+    return $string if $type =~ /\A(?:format|uuid)\z/;
+
+    $string .= $type eq 'revision'  ? $NL
+        : $self->has_prop_or_text() ? $NL x 2
+        :                             $NL ;
+
+    return $string;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+An C<SVN::Dump::Record> object represents a Subversion dump record.
+
+=head1 METHODS
+
+C<SVN::Dump> provides the following methods:
+
+=over 4
+
+=item new()
+
+Create a new empty C<SVN::Dump::Record> object.
+
+=item type()
+
+Return the record type, as guessed from its headers.
+
+The method dies if the record type cannot be determined.
+
+=item set_headers( $headers )
+
+=item get_headers()
+
+Get or set the C<SVN::Dump::Headers> object that represents the record
+headers.
+
+=item set_property( $property )
+
+=item get_property()
+
+Get or set the C<SVN::Dump::Property> object that represents the record
+property block.
+
+=item set_text( $text )
+
+=item get_text()
+
+Get or set the C<SVN::Dump::Text> object that represents the record
+text block.
+
+=item set_included( $record )
+
+=item get_included()
+
+Some special record are actually output recursiveley by B<svnadmin dump>.
+The "record in the record" is stored within the parent record, so they
+are parsed as a single record with an included record.
+
+C<get_record()> / C<set_record()> give access to the included record.
+
+According to the Subversion sources (F<subversion/libsvn_repos/dump.c>),
+this is a "delete original, then add-with-history" node. The dump looks
+like this:
+
+    Node-path: tags/mytag/myfile
+    Node-kind: file
+    Node-action: delete
+    
+    Node-path: tags/mytag/myfile
+    Node-kind: file
+    Node-action: add
+    Node-copyfrom-rev: 23
+    Node-copyfrom-path: trunk/myfile
+    
+    
+    
+    
+
+Note that there is a single blank line after the first header block,
+and four after the included one.
+
+=item has_prop()
+
+Return a boolean value indicating if the record has a property block.
+
+=item has_text()
+
+Return a boolean value indicating if the record has a text block.
+
+=item has_prop_only()
+
+Return a boolean value indicating if the record has only a property block
+(and no text block).
+
+=item has_prop_or_text()
+
+Return a boolean value indicating if the record has a property block
+or a text block.
+
+=item as_string()
+
+Return a string representation of the record.
+
+B<Warning:> dumping a record currenly gives back the information that
+was read from the original dump. Which means that if you modified the
+property or text block of a record, the headers will be inconstent.
+
+=back
+
+=head1 SEE ALSO
+
+C<SVN::Dump::Headers>, C<SVN::Dump::Property>, C<SVN::Dump::Text>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 Philippe 'BooK' Bruhat, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
