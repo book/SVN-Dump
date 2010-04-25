c26138c58d4738a09872e26aa2c37866fe62763c
Philippe Bruhat (BooK) <book@cpan.org>
2010-04-25 11:51:55 +0200

    SVN::Dump::Text: add a generic digest() method

--- lib/SVN/Dump/Text.pm
+++ lib/SVN/Dump/Text.pm
@@ -18,6 +18,14 @@ sub set {
 sub get { ${ $_[0] } }
 *as_string = \&get;
 
+sub digest {
+    my ( $self, $algo ) = @_;
+    return eval {
+        require Digest;
+        Digest->new( uc $algo )->add($$self)->hexdigest;
+    };
+}
+
 1;
 
 __END__
@@ -58,13 +66,20 @@ Set the text of the C<SVN::Dump::Text> object.
 
 Return a string representation of the text block.
 
+=item digest( $algo )
+
+Return a digest of the text computed with the C<$algo> algorithm in
+hexadecimal form. See the C<Digest> module for valid values of C<$algo>.
+
+Return C<undef> if the digest algorithm is not supported.
+
 =back
 
 =head1 SEE ALSO
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2006 Philippe 'BooK' Bruhat, All Rights Reserved.
+Copyright 2006-2010 Philippe 'BooK' Bruhat, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
